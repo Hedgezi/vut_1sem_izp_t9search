4f756cbf7393b0c42879f95c1e58b6647c709779
Hedgezi <nikola@rootnix.net>
2022-10-18 15:27:34 +0200

    trying to implement 4th point using lcs

--- t9search.c
+++ t9search.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <ctype.h> // tolower(), toupper()
 #include <stdbool.h>
+#define max(a,b) (a > b)? a : b
 
 /* checkNumbers
 Finds all fitted letters from substring in checked string, even if they have any symbols between them
@@ -25,6 +26,34 @@ bool checkNumbers(int lencs, char checkedstring[], int lenex, char example[]) {
             return true;
     }
     return false;
+}
+
+/* lcsfunc
+Finds longest common subsequence
+
+Arguments:
+    int lencs, char checkedstring[], int lenex, char example[]: 2 engths and 2 strings for which we will find LCS
+Returns: 
+*/
+int lcsfunc(int lencs, char checkedstring[], int lenex, char example[]) {
+    int lcs[lencs+1][lenex+1];
+    for (int i = 0; i < lencs+1; i++) {
+        lcs[i][0] = 0;
+    }
+    for (int j = 0; j < lenex+1; j++) {
+        lcs[0][j] = 0;
+    }
+    for (int i = 1; i < lencs+1; i++) {
+        for (int j = 1; j < lenex+1; j++) {
+            lcs[i][j] = (checkedstring[i] == example[j]) ? lcs[i-1][j-1] + 1 : max(lcs[i-1][j], lcs[i][j-1]);
+        }
+    }
+    printf("lcs: %i\n", lcs[lencs][lenex]);
+    return lcs[lencs][lenex];
+}
+
+bool levenstein(int lencs, char checkedstring[], int lenex, char example[], int posserr) {
+    return (lcsfunc(lencs, checkedstring, lenex, example) + posserr > lenex) ? false : true;
 }
 
 /* translateToNumbers
@@ -108,6 +137,7 @@ int main(int argc, char* argv[]) {
     char nameline[101];
     char telline[101];
     char namenumline[101];
+    int posserr = 2;
     bool atleastoneentry = false;
     // checking the number of given arguments in shell
     if (argc == 1) {
@@ -143,10 +173,15 @@ int main(int argc, char* argv[]) {
         }
         charsToLowercase(strlen(nameline), nameline);
         translateToNumbers(strlen(nameline), nameline, namenumline);
+        printf(": %s\n", namenumline);
         if (checkNumbers(strlen(namenumline), namenumline, strlen(argv[1]), argv[argc-1]) || checkNumbers(strlen(telline), telline, strlen(argv[1]), argv[1])){
             printf("%s, %s\n", nameline, telline);
             atleastoneentry = true;
         }
+        if (levenstein(strlen(namenumline), namenumline, strlen(argv[1]), argv[argc-1], posserr) || levenstein(strlen(telline), telline, strlen(argv[1]), argv[1], posserr)){
+            printf("%s, %s\n", nameline, telline);
+            atleastoneentry = true;
+        }        
     }
     if (!atleastoneentry) {
         printf("Not found\n");
